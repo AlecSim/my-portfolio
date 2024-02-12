2258876c13b0af79b391b4ac7fffd956b89773c8
simalecjericho <simalecjericho@gmail.com>
2024-02-12 18:03:10 +0800

    sample portfolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import { Inter } from "next/font/google";
-import Navbar from "@/components/Navbar";
-import Head from "next/head";
-import LeftSide from "@/components/Leftside";
 import About from "@/components/About";
 import Banner from "@/components/Banner";
+import Contact from "@/components/Contact";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
+
+import Navbar from "@/components/Navbar";
+import Projects from "@/components/Projects";
 import RightSide from "@/components/RightSide";
+import Head from "next/head";
+import { motion } from "framer-motion";
+import Archive from "@/components/Archive";
+import LeftSide from "@/components/Leftside";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,19 +24,34 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="font-bodyFont w-full h-screen bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
+      <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight  overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
         <Navbar />
         <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
-          <div className="w-32 h-full">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+            className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0"
+          >
             <LeftSide />
-          </div>
-          <div className="w-full h-[88vh] overflow-y-scroll scrollbarHide">
+          </motion.div>
+          <div className="h-[88vh] mx-auto p-4">
             <Banner />
             <About />
+            <Experience />
+            <Projects />
+            <Archive />
+            <Contact />
+            <Footer />
           </div>
-          <div className="w-32 h-full">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+            className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0"
+          >
             <RightSide />
-          </div>
+          </motion.div>
         </div>
       </main>
     </>
